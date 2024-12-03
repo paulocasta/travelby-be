637a8c1c78be395870406d09a28bea580db799d8
pcastaneira <Paulo.Castaneira@endava.com>
2024-12-03 16:15:52 -0300

    initial commit

---
+++ src/main/java/com/travelby/TravelByApplication.java
@@ -0,0 +1,13 @@
+package com.travelby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelByApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelByApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/travelby/TravelByApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelByApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
