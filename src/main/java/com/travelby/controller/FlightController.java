6ff84437380443ec32e6ace7b343069d6cd1983d
paulo <paulo.castaneira@gmail.com>
2024-12-26 16:48:28 -0300

    Add parameters to search by queries

--- src/main/java/com/travelby/controller/FlightController.java
+++ src/main/java/com/travelby/controller/FlightController.java
@@ -3,6 +3,7 @@ package com.travelby.controller;
 import com.travelby.dto.FlightDTO;
 import com.travelby.service.FlightService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,9 @@ public class FlightController {
     private final FlightService flightService;
 
     @GetMapping
-    public List<FlightDTO> getFlights() {
-        return flightService.getAllFlights();
+    public List<FlightDTO> getFlights(@Param("origin") String origin, @Param("destination") String destination,
+                                      @Param("departureDate") String departureDate, @Param("returnDate") String returnDate) {
+
+        return flightService.getAllFlights(origin, destination, departureDate, returnDate);
     }
 }
